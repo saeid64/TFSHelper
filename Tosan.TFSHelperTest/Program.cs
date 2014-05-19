0c95c907a8fda4c2d3b85f0d1b308e86eb57591f
Moosaei Saeed <saeid64@gmail.com>
2014-05-19 11:09:44 +0430

    Adding Just Simple Test Project

---
+++ Tosan.TFSHelperTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using Tosan.TFSHelper;
+using Tosan.TFSHelper.Model;
+
+namespace Tosan.TFSHelperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating instance of WorkItem Service
+            var workItemService = new WorkItemService()
+            {
+                //Optional Parameters:
+                //Credential = new NetworkCredential("SomeUser","Password","CompanyDomain"),
+                //ImpersonateeDomainName = "SomeUserDomainName",
+                //TFSServerUri = "http://YourTFSServer:8080/tfs", //like this project can be specified in application configuration as 'TFSServerUri' in appSettings section
+                //DefaultCollectionName = "YourDefaultProjectCollectionName" //like this project can be specified in application configuration as 'TFSCollectionName' in appSettings section
+            };
+            //Initiating Services
+            workItemService.Init();
+
+            //Now Calling some extension services
+
+            var globalListValues = workItemService.GetGlobalListValues("GlobalListName");
+
+            var workItem = workItemService.GetWorkItem(40579);
+
+            var tfsItem = workItemService.GetTFSItem(workItem);
+
+            var parentWorkItem = workItemService.GetParentWorkItem(40579, WorkItemLinkType.Topology.Tree);
+
+            var collections = workItemService.GetCollectionsName();
+
+        }
+    }
+}
